091ba4074c08096276df42f88a3b39c01070f707
philip581 <philipfernandezz@gmail.com>
2024-02-25 15:12:15 -0500

    create map and update movement

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    //public speed variable
+    public int speed;
+
+    //public animation variable
+    public Animator anim;
+
+    //if walking boolean
+    private bool isWalking = false;
+
+    //animation boolean
+    private bool isAnimating = false;
+
+    //direction variable
+    private string direction;
+    
+    //collision function 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isAnimating = false;
+    }
+
+    //function for moving in certain directions
+    private void MoveDirection(string direction)
+    {
+        //handle movement
+        if(direction == "up")
+        {
+            transform.position += new Vector3(0,speed,0) * Time.deltaTime;
+            transform.rotation = new Quaternion(0,0,180,0);
+        }
+        else if(direction == "left")
+        {
+            transform.position += new Vector3(-speed,0,0) * Time.deltaTime;
+            transform.rotation = new Quaternion(0,180,0,0);
+        }
+        else if(direction == "down")
+        {
+            transform.position += new Vector3(0,-speed,0) * Time.deltaTime;
+            transform.rotation = new Quaternion(0,0,0,0);
+        }
+        else if(direction == "right")
+        {
+            transform.position += new Vector3(speed,0,0) * Time.deltaTime;
+            transform.rotation = new Quaternion(0,0,0,0);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //handle movement
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            direction = "up";
+            isWalking = true;
+            isAnimating = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            direction = "left";
+            isWalking = true;
+            isAnimating = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            direction = "down";
+            isWalking = true;
+            isAnimating = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            direction = "right";
+            isWalking = true;
+            isAnimating = true;
+        }
+
+        //wrap around screen if out of bounds
+        if(transform.position.y > 7.3)
+        {
+            transform.position = new Vector3(transform.position.x,-5.6f,0);
+        }
+        if(transform.position.y < -5.6)
+        {
+            transform.position = new Vector3(transform.position.x,7.2f,0);
+        }
+        if(transform.position.x > 10.3)
+        {
+            transform.position = new Vector3(-10.5f,transform.position.y,0);
+        }
+        if(transform.position.x < -10.5)
+        {
+            transform.position = new Vector3(10.3f,transform.position.y,0);
+        }
+        
+        //move player
+        if(isWalking)
+        {
+            MoveDirection(direction);
+        }
+
+        //if walking, play walking animation, otherwise idle
+        if(isAnimating)
+        {
+            anim.Play("mister_cat_walk");
+        }
+        else
+        {
+            anim.Play("mister_cat_idle");
+        }
+    }
+}
